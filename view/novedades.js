c7fbf593a1b027c6d7c0baaad367c2ccb6c88127
Anabelle Chavez <anabellechavez@MacBook-Air-de-Anabelle.local>
2020-01-15 16:15:42 -0700

    tamaño 33%

--- view/novedades.js
+++ view/novedades.js
@@ -7,19 +7,25 @@
 var slideIndex = 1;
 showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+document.getElementById('siguiente').onclick = function(){
+  slideIndex+=1;
 }
+document.getElementById('anterior').onclick = function(){
+  slideIndex-=1;
+}
+
 
 function showDivs(n) {
   
   //variable x = fotos, variable y = texto (Esto es para la prueba, con imagenes y texto hardcodeado, ver como sacarlo de un query)
   var x = document.getElementsByClassName("SlideShow");
-  var y = document.getElementById("Texto");
+  var y = document.getElementsByClassName("testo");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length} ;
   for (var i = 0; i < x.length; i++) {
     x[i].style.display = "none";
+    y[i].style.display = "none";
   }
   x[slideIndex-1].style.display = "block";
+    y[slideIndex-1].style.display = "block";
 }
